b0e378142ad1c5d3af54cd3835a46286b36ffc81
james455333 <james455333@gmail.com>
2020-10-07 16:56:20 +0800

    new Base 20201007

---
+++ mountainEX_Project/src/main/java/mountain/mountainList/model/MountainBean.java
@@ -0,0 +1,49 @@
+package mountain.mountainList.model;
+
+public class MountainBean {
+	
+	String name ;
+	String description;
+	String advice; 
+	String traffic;
+	String npName;
+	String imgUrl;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAdvice() {
+		return advice;
+	}
+	public void setAdvice(String advice) {
+		this.advice = advice;
+	}
+	public String getTraffic() {
+		return traffic;
+	}
+	public void setTraffic(String traffic) {
+		this.traffic = traffic;
+	}
+	public String getNpName() {
+		return npName;
+	}
+	public void setNpName(String npName) {
+		this.npName = npName;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+
+}
---
+++ mountainEX_Project/src/main/webapp/tester/test.java
@@ -0,0 +1,8 @@
+package tester;
+
+public class test {
+	
+	
+	
+
+}
