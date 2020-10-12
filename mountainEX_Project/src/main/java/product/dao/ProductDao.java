98340ea6b28afa560d233e1a50399e5650443749
redupup <redgod3630@gmail.com>
2020-10-12 14:17:01 +0800

    Update ProductDao.java
    1012

--- mountainEX_Project/src/main/java/product/dao/ProductDao.java
+++ mountainEX_Project/src/main/java/product/dao/ProductDao.java
@@ -1,12 +1,18 @@
 package product.dao;
 
+import java.util.List;
+
 import product.model.ProductBean;
 
 public interface ProductDao {
-	// 依bookID來刪除單筆記錄
+	
+	//列出Products
+		List<ProductBean> getProducts();
+	
+	// 依name來刪除單筆記錄
 		int deleteProduct(String name);
 
-		// 依bookID來查詢單筆記錄
+		// 依name來查詢單筆記錄
 		ProductBean getProduct(String name);
 		
 		
