811c84138957fb0104f187c89e6fdb2853c4c74f
redupup <redgod3630@gmail.com>
2020-10-12 14:17:14 +0800

    Update ProductServlet.java
    1012

--- mountainEX_Project/src/main/java/product/controller/ProductServlet.java
+++ mountainEX_Project/src/main/java/product/controller/ProductServlet.java
@@ -9,14 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import product.model.ProductBean;
 import product.service.ProductService;
 import product.service.ProductServiceImpl;
 
-/**
- * Servlet implementation class ProductServlet
- */
 @WebServlet("/ProductServlet")
 public class ProductServlet extends HttpServlet  {
 	private static final long serialVersionUID = 1L;
@@ -40,17 +37,17 @@ public class ProductServlet extends HttpServlet  {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		HttpSession session = request.getSession(false);
+//		HttpSession session = request.getSession(false);
 		
-		ProductService service = new ProductService();
+		ProductService service = new ProductServiceImpl();
 		request.setAttribute("baBean", service);
 		//
 //		service.setPageNo(pageNo);
 //		service.setRecordsPerPage(GlobalService.RECORDS_PER_PAGE);
-//		Collection<BookBean> coll = service.getPageBooks();
+		Collection<ProductBean> coll = service.getProducts();
 //		request.setAttribute("pageNo", pageNo);
 //		request.setAttribute("totalPages", service.getTotalPages());
-//		request.setAttribute("products_DPP", coll);
+		request.setAttribute("products_DPP", coll);
 		// 交由listBooks.jsp來顯示某頁的書籍資料，同時準備『第一頁』、
 		// 『前一頁』、『下一頁』、『最末頁』等資料
 		RequestDispatcher rd = request.getRequestDispatcher("ProductMaintainList.jsp");
