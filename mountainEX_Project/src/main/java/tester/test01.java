5dea80c6ca77544f792219fb05345de303072d26
redupup <redgod3630@gmail.com>
2020-10-12 17:08:39 +0800

    Create test01.java
    1012

---
+++ mountainEX_Project/src/main/java/tester/test01.java
@@ -0,0 +1,15 @@
+package tester;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		List<String> tableNameList = new ArrayList<String>();
+	    tableNameList.add("cloth_class");
+	    tableNameList.add("shoes_class");
+	    System.out.println(tableNameList);
+	}
+
+}
